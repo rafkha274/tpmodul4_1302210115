a5c01cb0f2728e78883f759b5ae4ac9e03349233
rafkha <mrafkha@student.telkomuniversity.ac.id>
2023-03-12 22:14:02 +0800

    inisialisasi projek

---
+++ tpmodul4_1302210115/tpmodul4_1302210115/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace tpmodul4_1302210115
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            KodePos table_Kodepos = new KodePos();
+            Console.WriteLine("==== All Post Code ====");
+            table_Kodepos.getAllkodepos();
+
+            Console.WriteLine("==== DOOR KEY ====");
+
+
+        }
+
+    }
+}
